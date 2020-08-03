b8cb13b72449e46cdfa78322492e7ef32162b634
maxnoller <max.noller1@gmail.com>
2020-08-03 20:40:19 +0200

    added performance enhancement to helper functions also average fps print

--- gui/helper_functions.py
+++ gui/helper_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 
 class GuiHelperFunctions:
     @staticmethod
@@ -11,20 +12,24 @@ class GuiHelperFunctions:
 
     @staticmethod
     def convert_creature_cords(coordinates, alpha):
-        return_coordinates = []
-        for (x,y) in coordinates:
-            (rho, phi) = GuiHelperFunctions.cart2pol(x,y)
-            return_coordinates.append(GuiHelperFunctions.pol2cart(rho,phi+np.deg2rad(alpha)))
-        return return_coordinates
+        return [GuiHelperFunctions.convert_creature_cord(x, y, alpha) for (x, y) in coordinates]
 
     @staticmethod
+    def convert_creature_cord(x, y, alpha):
+        (rho, phi) = GuiHelperFunctions.cart2pol(x, y)
+        return GuiHelperFunctions.pol2cart(rho, phi+np.deg2rad(alpha))
+
+
+    @staticmethod
+    @jit(nopython=True)
     def cart2pol(x, y):
-        rho = np.sqrt(x**2 + y**2)
+        rho = np.sqrt(np.square(x) + np.square(y))
         phi = np.arctan2(y, x)
-        return(rho, phi)
+        return (rho, phi)
 
     @staticmethod
+    @jit(nopython=True)
     def pol2cart(rho, phi):
-        x = rho * np.cos(phi)
+        x = np.multiply(rho, np.cos(phi))
         y = rho * np.sin(phi)
         return(x, y)
