0e627d1528cd999694ab58ce57c5f8c6b0af779f
maxnoller <max.noller1@gmail.com>
2020-08-03 20:45:45 +0200

    optimised network optimization functions with jit

--- brain/activation_functions.py
+++ brain/activation_functions.py
@@ -1,8 +1,11 @@
 import numpy as np
 import math
+from numba import jit
 
+@jit(nopython=True)
 def ReLU(input):
     return np.maximum(0, input)
 
+@jit(nopython=True)
 def Sigmoid(input):
     return 1/(1+math.exp(-input))
